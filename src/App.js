759549df191873e64f426cf5f307de917485d308
yruiu <miskouran0503@gmail.com>
2022-10-15 13:08:33 +0300

    csdassd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import Header from "./components/Header/Header";
 // window.Telegram.WebApp()
+const tg=window.Telegram.WebApp;
 function App(){
   useEffect(()=>{
     tg.ready();
