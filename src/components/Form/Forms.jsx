eb795d507671843cfed67710062a5f81d3464580
yruiu <miskouran0503@gmail.com>
2022-10-16 15:23:16 +0300

    csdassdcxsd

--- src/components/Form/Forms.jsx
+++ src/components/Form/Forms.jsx
@@ -1,11 +1,25 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import './Form.css'
 import {useTelegram} from "../hooks/useTelegram";
 const Forms = () => {
     const [country, setCountry]=useState('');
-    const [city, setCity]=useState('');
+    const [street, setStreet]=useState('');
     const [subject, setSubject]=useState('physical');
     const {tg} = useTelegram()
+    const onSendData=useCallback(()=>{
+        const data={
+            country,
+            street,
+            subject
+        }
+        tg.sendData(JSON.stringify(data))
+    }, [])
+    useEffect(()=>{
+        tg.onEvent('mainButtonClicked', onSendData())
+        return()=>{
+            tg.offEvent('mainButtonClicked', onSendData())
+        }
+    })
     useEffect(()=>{
         tg.MainButton.setParams({
             text: "Upload data"
@@ -24,15 +38,15 @@ const Forms = () => {
     const onChangeSubject=(e)=>{
         setSubject(e.target.value)
     }
-    const onChangeCity=(e)=>{
-        setCity(e.target.value)
+    const onChangeStreet=(e)=>{
+        setStreet(e.target.value)
     }
     return (
         <div>
             <div className="form">
                 <h3>Input your data</h3>
                 <input type="text" value={country} onChange={onChangeCountry} className="input" placeholder={'Country'}/>
-                <input type="text" value={city} onChange={onChangeCity} className="input" placeholder={'Street'}/>
+                <input type="text" value={street} onChange={onChangeStreet} className="input" placeholder={'Street'}/>
                 <select value={subject} onChange={onChangeSubject} className="select">
                     <option value="physical">Physical person</option>
                     <option value="legal">legal person</option>
